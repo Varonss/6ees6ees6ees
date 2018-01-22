8bc5069bcf4e6ac600b3588824f22e30f40bc167
Varonss <martin.varona.84@gmail.com>
2018-01-22 16:40:06 -0600

    done with class honeyMakerBee using extand from Grub.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Grub{
+  constructor(){
+  super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.color = 'yellow'
+    this.honeyPot = 0;
+}
+makeHoney(){
+  this.honeyPot++;
+  return this.honeyPot;
+}
+giveHoney(){
+  if(this.honeyPot){
+    this.honeyPot--;
+  }
+  return this.honeyPot;
+ }
+}
