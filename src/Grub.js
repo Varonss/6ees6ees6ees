08a0542398058a22d2f846bbd8449dde56ec72c6
Varonss <martin.varona.84@gmail.com>
2018-01-22 15:23:08 -0600

    Done making a constructor for class

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,12 @@
 class Grub {
-};
+  //call a constructor
+  constructor() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  }
+  //add a method
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
+}
